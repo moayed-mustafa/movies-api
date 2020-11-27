4a1c121e1726c5254858d37b236251042244554a
moayed-mustafa <moayed.abrida@yahoo.com>
2020-11-27 17:14:22 +0400

    removed some whitespace

--- model.js
+++ model.js
@@ -1,45 +1,40 @@
 
-/**Data base Logic */
+/**Database Logic */
 
 const db = require('./db');
 class MovieModel{
     static async vote(delta, direction, title) {
-
-
         const movieExist = await db.query(`SELECT * FROM movies WHERE title = $1`, [title])
         if (movieExist.rows.length === 0) {
             if (direction == 'up') {
                 await db.query(
                     `INSERT INTO movies
-                        (title, thumbs_up)
-                        VALUES ($1, $2)
-                    `,
-               [title, delta] )
+                    (title, thumbs_up)
+                    VALUES ($1, $2)
+                    `, [title, delta])
+
             } else {
                 await db.query(
                     `INSERT INTO movies
-                        (title, thumbs_down)
-                        VALUES ($1, $2)
-                    `,
-               [title, 1])
+                    (title, thumbs_down)
+                    VALUES ($1, $2)
+                    `, [title, 1])
             }
-
-
         } else {
             if (direction == "up") {
                 await db.query(`
                     UPDATE movies
                     SET thumbs_up = thumbs_up + $1
-                    WHERE title = $2
-                `, [delta, title])
+                    WHERE title = $2`, [delta, title])
+
             } else {
                 await db.query(`
                     UPDATE movies
                     SET thumbs_down = thumbs_down - $1
-                    WHERE title = $2
-                `, [delta, title])
+                    WHERE title = $2`, [delta, title])
             }
-            }
+        }
+
     }
 
     static async getVotes(title) {
